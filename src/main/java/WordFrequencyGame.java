d8322dfdf6615aab35ff49f769077e753dc0b597
real-chiu <real.chiu@oocl.com>
2020-04-14 15:12:37 +0800

    rename variables/function

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,61 +9,57 @@ public class WordFrequencyGame {
     public static final String SPACE_PATTERN = "\\s+";
     public static final String DELIMITER = "\n";
 
-    public String getResult(String inputStr) {
+    public String getResult(String sentence) {
 
 
-        if (inputStr.split(SPACE_PATTERN).length == 1) {
-            return inputStr + " 1";
+        if (sentence.split(SPACE_PATTERN).length == 1) {
+            return sentence + " 1";
         } else {
-
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split(SPACE_PATTERN);
+                String[] words = sentence.split(SPACE_PATTERN);
 
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
+                List<WordInfo> wordInfoList = new ArrayList<>();
+                for (String word : words) {
+                    WordInfo wordInfo = new WordInfo(word, 1);
+                    wordInfoList.add(wordInfo);
                 }
 
                 //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
+                Map<String, List<WordInfo>> wordInfoMap = getMap(wordInfoList);
 
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
+                List<WordInfo> list = new ArrayList<>();
+                for (Map.Entry<String, List<WordInfo>> entry : wordInfoMap.entrySet()) {
+                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
+                    list.add(wordInfo);
                 }
-                inputList = list;
+                wordInfoList = list;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfoList.sort((firstWord, secondWord) -> secondWord.getWordCount() - firstWord.getWordCount());
 
                 StringJoiner joiner = new StringJoiner(DELIMITER);
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
+                for (WordInfo w : wordInfoList) {
+                    String wordWithCount = w.getWord() + " " + w.getWordCount();
+                    joiner.add(wordWithCount);
                 }
                 return joiner.toString();
-            } catch (Exception e) {
+            } catch (Exception exception) {
                 return "Calculate Error";
             }
         }
     }
 
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+    private Map<String, List<WordInfo>> getMap(List<WordInfo> wordInfoList) {
+        Map<String, List<WordInfo>> wordInfoMap = new HashMap<>();
+        for (WordInfo wordInfo : wordInfoList) {
+            if (!wordInfoMap.containsKey(wordInfo.getWord())) {
+                ArrayList wordInfoArray = new ArrayList<>();
+                wordInfoArray.add(wordInfo);
+                wordInfoMap.put(wordInfo.getWord(), wordInfoArray);
             } else {
-                map.get(input.getValue()).add(input);
+                wordInfoMap.get(wordInfo.getWord()).add(wordInfo);
             }
         }
-        return map;
+        return wordInfoMap;
     }
 }
